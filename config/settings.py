9df878ecaabb2189e7fc651f35ef2411349052cc
sugwanlee <austinlee.devv@gmail.com>
2025-05-22 11:11:41 +0900

    DEBUG 상태에 따라 데이터베이스 설정 조건을 수정함

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
